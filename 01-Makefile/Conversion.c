9ae9f7887835e838da1ded639da4f8184716c7bf
Kevin Alá <kevin.ala492@gmail.com>
2023-06-16 16:43:02 -0300

    Ahora está mejor

--- 01-Makefile/Conversion.c
+++ 01-Makefile/Conversion.c
@@ -1,5 +1,36 @@
+#include <stdio.h>
 #include "Conversion.h"
 
-double GetCelsFromFahr(double f){
- return (5.0/9.0)*(f-32);
+void Farenheit(int upper, int lower, int step){
+    int fahr, celsius;
+    fahr = lower;
+    printf("%s\t%s\n", "fahr", "celsius");
+    while(fahr<=upper){
+        celsius = 5 * (fahr-32)/9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
+
+void Celsius(int upper, int lower, int step){
+    int fahr, celsius;
+    celsius = lower;
+    printf("%s\t%s\n", "celsius", "fahr" );
+    while(celsius<=upper){
+        fahr = (celsius*9/5)+32;
+        printf("%d\t%d\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+}
+
+int main(){
+    int upper, lower, step;
+    upper = 300;
+    lower = 0;
+    step = 20;
+    
+    Farenheit(upper, lower, step);
+    
+    Celsius(upper, lower, step);
+    return 0;
 }
